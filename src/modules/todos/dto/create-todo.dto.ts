37a198ad0bfa6f84cc207dcb86b38b4ff316f294
trantuankiet0518-byte <trantuankiet0518@gmail.com>
2026-05-03 23:22:51 +0700

    fix: add validation decorators to CreateTodoDto

--- src/modules/todos/dto/create-todo.dto.ts
+++ src/modules/todos/dto/create-todo.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
 }
